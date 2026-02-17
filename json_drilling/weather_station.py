4bf1a83947d12b340ba49ff0cf3b549df7fd8d34
kanishikaverma506 <kanishikaverma8@gmail.com>
2026-02-17 22:11:26 +0530

    more drilling

---
+++ json_drilling/weather_station.py
@@ -0,0 +1,27 @@
+#data
+data={
+  "region": "Europe",
+  "stations": [
+    {
+      "station_id": "LDN-01",
+      "city": "London",
+      "readings": [
+        {"timestamp": "10:00", "temp": 15, "humidity": 80},
+        {"timestamp": "11:00", "temp": 17, "humidity": 75}
+      ]
+    },
+    {
+      "station_id": "PAR-02",
+      "city": "Paris",
+      "readings": [
+        {"timestamp": "10:00", "temp": 19, "humidity": 60}
+      ]
+    }
+  ]
+}
+
+#get 2nd temp reading of london
+
+print(f"second reading temperatue  in london:{data['stations'][0]['readings'][1]['temp']}\n")
+#get humidity of paris
+print(f"humidity in paris:{data['stations'][1]['readings'][0]['humidity']}")
