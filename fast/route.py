8f6fe85613f60130e9abde99c4aacbdb4ac4bcf7
kanishikaverma506 <kanishikaverma8@gmail.com>
2026-04-26 06:00:13 +0530

    required libraries

--- fast/route.py
+++ fast/route.py
@@ -1,10 +1,4 @@
-from fastapi import fastAPI
-app=fastAPI()
-
-@app.get("/")
-def home():
-    return {"message":"hello , am home!"}
-
-@app.get("/status")
-def status():
-    return{"database":"connected", "server":"online"}
+import fastapi
+import pandas 
+import requests
+print ("Everything is installed and ready")
